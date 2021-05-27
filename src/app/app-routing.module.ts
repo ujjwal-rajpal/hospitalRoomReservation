3e7137e786e15d34f8d0b17c3432cb69ea103750
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2021-05-27 23:46:45 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+/**
+ * filename : app-routing.module.ts
+ * purpose: used to route module
+ * created by : ujjwal
+ * created on: 27 May 2021
+ */
+
+import {NgModule} from "@angular/core";
+
+@NgModule({
+
+})
+
+export class AppRoutingModule{}
